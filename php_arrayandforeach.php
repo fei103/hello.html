06989df96a3fd18e0d88d538adbf126356d10183
fei103 <106908282+fei103@users.noreply.github.com>
2022-06-06 01:53:55 +0800

    Add files via upload

---
+++ php_arrayandforeach.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>This is the title</title>
+	<head>
+	<body>
+   		<?php
+			$member = array("Alice", "Bob", "Charlie", "Daniel", "Elsa");
+		
+			foreach ($member as $name) {
+				echo "$name <br>";
+			}
+		?>
+	</body>
+</html>
